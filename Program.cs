a274a1e73f3a1cadaae8ab507801fba0a78f1385
VGDimitrov <59871151+VGDimitrov@users.noreply.github.com>
2023-03-31 08:36:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Dictinary zad 1 dom 
+ */
+
+string[] inputString=Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).ToArray();
+Dictionary<char,int>   countWords = new Dictionary<char,int>();
+int count = 0;
+for (int i = 0; i < inputString.Length; i++)
+{
+ 
+        foreach (char c in inputString[i])
+        {
+            if (countWords.ContainsKey(c))
+            {
+                if (countWords.ContainsKey(c)) 
+                {
+                countWords[c]+=1;
+
+                }
+                
+            }
+            else
+            {
+                countWords.Add(c, 1);
+            }
+
+        }
+    
+}
+foreach (var c in countWords)
+{
+    Console.WriteLine($"{c.Key} -> {c.Value}");
+}
